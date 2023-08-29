e046d6e7b1eb5b3bfe67b36fc2bf2a59e8b9496a
babu97 <benkaimugul@gmail.com>
2023-08-29 15:46:50 +0300

    ss

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,21 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    
+
+    from .admin import admin as admin_blueprint
+
+    app.register_blueprint(admin_blueprint)
+    from .carts import carts as carts_blueprint
+
+    app.register_blueprint(carts_blueprint)
+    from .customers import customers as customers_blueprint
+
+    app.register_blueprint(customers_blueprint)
+
+    from .products import products as products_blueprint
+
+    app.register_blueprint(products_blueprint)
+    from .shop import shop as shop_blueprint
+
+    app.register_blueprint(shop_blueprint)
+    return app
--- app/carts/__init__.py
+++ app/carts/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
 
 carts = Blueprint("carts", __name__)
+from .import views
