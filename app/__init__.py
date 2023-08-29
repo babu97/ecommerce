af46ddcd716a9e047b5db20b40b094a800942124
babu97 <benkaimugul@gmail.com>
2023-08-29 17:25:17 +0300

    more pythhon

--- app/__init__.py
+++ app/__init__.py
@@ -12,17 +12,17 @@ def create_app(config_name):
 
     from .admin import admin as admin_blueprint
 
-    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(admin_blueprint, url_prefix="/admin")
     from .carts import carts as carts_blueprint
 
-    app.register_blueprint(carts_blueprint)
+    app.register_blueprint(carts_blueprint, url_prefix="/carts")
     from .customers import customers as customers_blueprint
 
-    app.register_blueprint(customers_blueprint)
+    app.register_blueprint(customers_blueprint, url_prefix="/customers")
 
     from .products import products as products_blueprint
 
-    app.register_blueprint(products_blueprint)
+    app.register_blueprint(products_blueprint, url_prefix="/products")
     from .shop import shop as shop_blueprint
 
     app.register_blueprint(shop_blueprint)
--- app/shop/views.py
+++ app/shop/views.py
@@ -1,1 +1,8 @@
+from flask import render_template, session, redirect, request, url_for
 from . import shop
+
+shop.route("/")
+
+
+def home():
+    return "Home page of your shop"
