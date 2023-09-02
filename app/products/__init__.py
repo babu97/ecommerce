d88b9d1d347ada533b7dc5cc384eadaf4d1e0308
babu97 <benkaimugul@gmail.com>
2023-09-03 00:51:26 +0300

    paying

--- app/products/__init__.py
+++ app/products/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
 
 products = Blueprint("products", __name__)
+from . import view
--- app/products/view.py
+++ app/products/view.py
@@ -3,6 +3,8 @@ from flask import redirect, render_template, url_for, flash, request
 from app.models import db
 from .form import nameForm
 from app.models import Brand
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError  # Import IntegrityError from SQLAlchemy
 
 
 @products.route("/addbrand", methods=["GET", "POST"])
@@ -10,9 +12,13 @@ def addbrand():
     form = nameForm()
     if form.validate_on_submit():
         brand = Brand(name=form.name.data)
-        db.session.add(brand)
-        db.session.commit()
-        flash("Brand added succesfully", category="success")
-        return redirect(url_for("addbrand"))
-
-    return render_template("products/addbrand.html")
+        try:
+            db.session.add(brand)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()  # Rollback the transaction in case of IntegrityError
+            flash("A brand with the same name already exists.", category="warning")
+        else:
+            flash("Brand added successfully", category="success")
+            return redirect(url_for(".addbrand"))
+    return render_template("products/addbrand.html", form=form)
